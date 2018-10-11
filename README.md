18764e34b26b1da196c4edbf1c5d968ab7a15fd8
Stefan Kamsker <stefan.kamsker@virtual-identity.com>
2018-10-11 11:37:40 +0200

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# example-image-resizing
-example S3 storage and image resizing service
+unite cms graphQl and responsive image resizing example
+================================================
+
+This example shows how to fetch image data from unite-cms over GraphQL and shows some responsive image resizing with 2 image resize Services (cloudimage, thumbor). 
+It's written in poor PHP and is using https://github.com/euautomation/graphql-client as graphQL client.
+
+## Installation
+
+    1) git clone https://github.com/unite-cms/example-image-resizing.git
+
+    2) cd example-image-resizing
+
+    3) composer install
+
+    4) edit index.php and change creditionals
+
+    5) open index-php in browser
---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+
+include_once 'vendor/autoload.php';
+
+use EUAutomation\GraphQL\Client;
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Unite CMS image resizing Example</title>
+    </head>
+
+<body>
+
+<?php
+
+$headers = [];
+
+# unite cms credentials
+$unite_cms_endpoint = 'https://masterking.unitecms.io/mydomain/api';
+$unite_cms_api_token = 'G3YUO2jlCsPKI4hcJV80JHIMQxVXmpd3HI76LqHk0p4';
+
+$query = '{
+    findPage {
+      result {
+        headline,
+        image {
+          type,
+          id,
+          size,
+          name,
+          url
+        }
+      }
+    }
+}';
+
+$client = new Client($unite_cms_endpoint);
+
+try 
+{
+    $response = $client->response($query, [], ['Authorization' => $unite_cms_api_token])->all();
+    $data = array_shift($response->findPage->result);
+
+    # thumbor is not happy with url schemes, so remove it
+    $thumbor_image_url = parse_url($data->image->url, PHP_URL_HOST).parse_url($data->image->url, PHP_URL_PATH);
+
+    ?>
+        <h1><?php print $data->headline ?></h1>
+
+        <h2>Thumbor Example</h2>
+
+        <picture>
+            <source media="(max-width: 480px)" srcset="https://my_thumbor.com/unsafe/480x0/<?php print $thumbor_image_url ?>">
+            <source media="(max-width: 768px)" srcset="https://my_thumbor.com/unsafe/768x0/<?php print $thumbor_image_url ?>">
+            <img src="https://my_thumbor.com/unsafe/1200x0/<?php print $thumbor_image_url ?>" alt="My Image" style="width:auto;">
+        </picture>
+
+        <hr />
+
+        <h2>cloudimage.io Example</h2>
+        
+        <picture>
+            <source media="(max-width: 480px)" srcset="https://xxxxxxx.cloudimg.io/width/480/x/<?php print $data->image->url ?>">
+            <source media="(max-width: 768px)" srcset="https://xxxxxxx.cloudimg.io/width/768/x/<?php print $data->image->url ?>">
+            <img src="https://xxxxxx.cloudimg.io/width/1200/x/<?php print $data->image->url ?>" alt="My Image" style="width:auto;">
+        </picture>
+
+    <?php
+
+    
+} catch (\Exception $e) {
+    
+    die('Error fetching data: '.$e->getMessage());
+}
+?>
+
+</body>
+</html> 
